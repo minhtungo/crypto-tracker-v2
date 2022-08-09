07b71b3f78f1492f73261264da18b7d831a25335
minhtungo <loducfc@gmail.com>
2022-08-08 21:28:22 -0400

    Set Up Coin Item and CoinSearch components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './components/Navbar';
 
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Account from './pages/Account';
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=15&page=1&sparkline=true';
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      const response = await axios.get(url);
+      setCoins(response.data);
+    };
+    fetchCoins();
+  }, []);
+
   return (
     <ThemeProvider>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home coins={coins} />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
     </ThemeProvider>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import CoinSearch from '../components/CoinSearch';
+
+const Home = ({ coins }) => {
+  return (
+    <div>
+      <CoinSearch coins={coins} />
+    </div>
+  );
+};
+export default Home;
